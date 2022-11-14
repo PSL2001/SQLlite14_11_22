b097c5615a6aa505567cf0e07852898e698809a1
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2022-11-14 14:10:28 +0100

    Comienzo proyecto: creado adapter

--- app/src/main/java/com/example/sqllite14_11_22/adapter/UsuariosAdapter.kt
+++ app/src/main/java/com/example/sqllite14_11_22/adapter/UsuariosAdapter.kt
@@ -1,17 +1,22 @@
 package com.example.sqllite14_11_22.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sqllite14_11_22.R
 import com.example.sqllite14_11_22.models.Usuarios
 
 class UsuariosAdapter(private val lista: MutableList<Usuarios>): RecyclerView.Adapter<UsuariosViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsuariosViewHolder {
-
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.usuario_layout, parent ,false)
+        return UsuariosViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: UsuariosViewHolder, position: Int) {
+        holder.render(lista[position])
     }
 
     override fun getItemCount(): Int {
+        return lista.size
     }
 }
--- app/src/main/java/com/example/sqllite14_11_22/adapter/UsuariosViewHolder.kt
+++ app/src/main/java/com/example/sqllite14_11_22/adapter/UsuariosViewHolder.kt
@@ -2,7 +2,15 @@ package com.example.sqllite14_11_22.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sqllite14_11_22.databinding.UsuarioLayoutBinding
+import com.example.sqllite14_11_22.models.Usuarios
 
 class UsuariosViewHolder(v: View): RecyclerView.ViewHolder(v) {
+    private val binding = UsuarioLayoutBinding.bind(v)
 
+    fun render(usuario: Usuarios) {
+        binding.tvId.text = usuario.id.toString()
+        binding.tvNombre.text = usuario.nombre
+        binding.tvEmail.text = usuario.email
+    }
 }
